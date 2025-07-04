8d327aa3ce5e0587c3a3a605f5223288f5339979
anderzon <anderzonportales@gmail.com>
2025-07-03 19:03:39 -0500

    Agregue libreia de Firebase Realtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Otras dependencias...
+    implementation("com.google.firebase:firebase-database:20.3.0")
 }
